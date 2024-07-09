fef6bd1bbcfb9a46f705348e6abf70fc420045d6
Zeeward41 <zeeward41@gmail.com>
2024-07-09 10:01:30 +0200

    feat: create register route logic

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,18 @@
 import asyncHandler from "../middleware/async.js"
 import User from '../models/User.js'
+import ErrorResponse from "../utils/errorResponse.js"
 
 // @desc        Register User
 // @route       POST /api/v1/auth/register
 //access        Public
 export const register = asyncHandler(async (req, res, next) => {
-    console.log(req.body)
+    const { name, email, password } = req.body
+
+    const newUser = await User.create({
+        name,
+        email,
+        password
+    })
     res.status(200).json({success: true})
 
 })
