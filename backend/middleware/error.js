19a4fc12c8adfd71ba7d7e45778e472a07c00165
Zeeward41 <zeeward41@gmail.com>
2024-07-09 10:47:23 +0200

    feat: handle mongoose duplicate field error (user already exists)

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,10 +1,23 @@
+import ErrorResponse from "../utils/errorResponse.js"
+
 const errorHandler = (err, req, res, next) => {
+
+    let error = {...err}
+
+    error.message = err.message
+
     //Log to console for dev
     console.log(err.stack.red)
 
-    res.status(err.statusCode || 500).json({
+    // Mongoose duplicate key
+    if(err.code === 11000) {
+        const message = `Duplicate field value entered`
+        error = new ErrorResponse(message, 400)
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     })
 }
 
