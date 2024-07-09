8c7d25644992de5e126493a5b6c2b3442308860e
Zeeward41 <zeeward41@gmail.com>
2024-07-09 20:45:27 +0200

    feat: Add login route functionality (+ install express-session)

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../middleware/async.js"
 import User from '../models/User.js'
 import ErrorResponse from "../utils/errorResponse.js"
+import bcrypt from 'bcryptjs'
 
 // @desc        Register User
 // @route       POST /api/v1/auth/register
@@ -15,4 +16,37 @@ export const register = asyncHandler(async (req, res, next) => {
     })
     res.status(201).json({success: true})
 
+})
+
+
+// @desc        Login User
+// @route       POST /api/v1/auth/login
+//access        Public
+export const login = asyncHandler(async (req, res, next) => {
+    const {email, password} = req.body
+
+    // Validate email and password
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400))
+    }
+
+    // Check for user
+    const user = await User.findOne({email: email}).select('+password')
+
+    if(!user) {
+        return next(new ErrorResponse(`User not found. Please check your email and try again.`, 404))
+    }
+
+    // Check if password Matches
+    // use Await because compare is a Promise
+    const checkPassword = await bcrypt.compare(password ,user.password)
+    console.log(`checkPassword : ${checkPassword}`)
+
+    if (!checkPassword) {
+        return next(new ErrorResponse(`Invalid credentials. Please check your password and try again.`, 401))
+    }
+
+    console.log(user)
+    
+    res.status(200).json({success: true})
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { register } from '../controllers/auth.js'
+import { register, login } from '../controllers/auth.js'
 import validateRegister from '../middleware/authValidation.js'
 
 const router = express.Router()
 
 router.post('/register', validateRegister, register)
+router.post('/login', login)
 
 export default router
