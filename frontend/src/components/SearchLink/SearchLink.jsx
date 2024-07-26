3bd6f8b6c663becb86695476165427c213eaeb83
Zeeward41 <zeeward41@gmail.com>
2024-07-26 15:07:05 +0200

    feat: creation SearchLink component

---
+++ frontend/src/components/SearchLink/SearchLink.jsx
@@ -0,0 +1,12 @@
+import './SearchLink.css'
+
+export default function SearchLink() {
+    return (
+        <form className='search-form'>
+            <input className='search-input' type='text' placeholder='Search URL' />
+            <button className='search-button'>
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </form>
+    )
+}
