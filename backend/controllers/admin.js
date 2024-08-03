51656daf17330939b4dbe15a25e72fc4c9bcb263
Zeeward41 <zeeward41@gmail.com>
2024-08-03 19:48:34 +0200

    feat: create admin-only route to fetch all minify URLs for a specific user by email

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/async.js"
 import User from '../models/User.js'
+import linkUrl from "../models/linkUrl.js"
 import ErrorResponse from "../utils/errorResponse.js"
 
 // @desc        Get all users
@@ -26,11 +27,33 @@ export const getUsers = asyncHandler(async (req, res, next) => {
 }) 
 
 
-
-// @desc        Get a specific minify URL from an user.
-// @route       GET /api/v1/users/:email/minify/:alias
-//access        Admin 
-
 // @desc        Get all minify URL for a specific user
 // @route       GET /api/v1/users/:email/minify/
 //access        Admin 
+export const getUserUrls = asyncHandler(async (req, res, next) => {
+    const user = req.user
+    const email = req.params.email
+
+    if (user.role !== 'admin') {
+        return next(new ErrorResponse(`Not authorized!`, 403))
+    }
+
+    const userTarget = await User.findOne({email})
+
+    if (!userTarget) {
+        return next(new ErrorResponse(`No user found with this email : ${email}`, 404))
+    }
+
+    const links = await linkUrl.find({user: userTarget._id})
+
+    if (!links || links.length === 0) {
+        return next(new ErrorResponse(`No Minify Url found for this user`, 404))
+    }
+
+    return res.status(200).json({
+        success: true,
+        count: links.length,
+        data: links
+    })
+})
+
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getUsers } from '../controllers/admin.js'
+import { getUsers, getUserUrls } from '../controllers/admin.js'
 import { protect, authorize } from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/', protect, authorize('admin'), getUsers)
+router.get('/:email/minify', protect, authorize('admin'), getUserUrls)
 
 export default router
