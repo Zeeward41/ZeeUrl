7146ab40f973ec4b26ef210b6c63b03a3d5bb4bc
Zeeward41 <zeeward41@gmail.com>
2024-08-08 19:14:18 +0200

    Add clipboard copy functionality for minified URL

--- frontend/src/pages/Dashboard/MinifyUrl/MinifyUrl.jsx
+++ frontend/src/pages/Dashboard/MinifyUrl/MinifyUrl.jsx
@@ -5,33 +5,41 @@ import {useOnClickOutside} from '../../../hooks/useOnClickOutside.jsx'
 import { format } from 'date-fns'
 
 export default function MinifyUrl({linkOriginal, token, numVisit, createdAt}) {
+    const [showPanel, setShowPanel] = useState(false)
+    const shortUrl = `https://www.zeeurl.com/${token}`
+
     const options = [
-        {'text': 'Copy Link','icon': 'fa-solid fa-copy'},
-        {'text': 'Edit Link', 'icon': 'fa-solid fa-pen-to-square'},
-        {'text': 'Stats', 'icon': 'fa-solid fa-chart-simple'},
-        {'text': 'Reset Stats', 'icon': 'fa-solid fa-rotate-left'},
-        {'text': 'Delete Link', 'icon': 'fa-solid fa-trash'}
+        {text: 'Copy Link',icon: 'fa-solid fa-copy', action: copyUrlToClipboard, setShowPanel: setShowPanel},
+        {text: 'Edit Link', icon: 'fa-solid fa-pen-to-square'},
+        {text: 'Stats', icon: 'fa-solid fa-chart-simple'},
+        {text: 'Reset Stats', icon: 'fa-solid fa-rotate-left'},
+        {text: 'Delete Link', icon: 'fa-solid fa-trash'}
     ] 
 
-    const [showPanel, setShowPanel] = useState(false)
+    
 
     function handleClick() {
         setShowPanel(!showPanel)
     }
     
     const refPanel = useRef()
+    const refButton = useRef()
 
-    useOnClickOutside(refPanel, () => {
+    useOnClickOutside([refPanel, refButton], () => {
         setShowPanel(false)
     })
+
+    function copyUrlToClipboard() {
+        navigator.clipboard.writeText(shortUrl)
+    }
 
     return (
         <div className='url-container'>
             <div className='left-box'>
                 <div className='url-qr'>
                     <i className="fa-solid fa-qrcode"></i>
-                    <a href={`https://zeeurl.com/${token}`} target="_blank" className='minify-url'>{token > 12 ? `zeeurl.com/${token.slice(0,12)}...`: `zeeurl.com/${token}`}</a>
-                    <i className="fa-solid fa-copy"></i>
+                    <a href={shortUrl} target="_blank" className='minify-url'>{token.length > 12 ? `zeeurl.com/${token.slice(0,12)}...`: `zeeurl.com/${token}`}</a>
+                    <i className="fa-solid fa-copy" onClick={copyUrlToClipboard}></i>
                 </div>
             <a href={linkOriginal} target="_blank" className='original-url'>{linkOriginal.length > 30 ? `${linkOriginal.slice(0,30)}...` : linkOriginal}</a>
             </div>
@@ -41,12 +49,12 @@ export default function MinifyUrl({linkOriginal, token, numVisit, createdAt}) {
                     <i className="fa-solid fa-chart-simple"></i>
                 </div>
                 <span className='creation-date'>{format(new Date(createdAt), 'dd-MM-yyyy')}</span>
-                <button onClick={handleClick} ref={refPanel} className='url-config'><i className="fa-solid fa-gear"></i><i className="fa-solid fa-caret-down"></i></button>
+                <button onClick={handleClick} ref={refButton} className='url-config'><i className="fa-solid fa-gear"></i><i className="fa-solid fa-caret-down"></i></button>
                 {showPanel ? 
-                <div className='option-config-panel'>
-                {options.map(({text, icon}) => {
+                <div className='option-config-panel' ref={refPanel}>
+                {options.map(({text, icon, action, setShowPanel}) => {
                     return (
-                        <OptionConfigLink key={text} text={text} icon={icon} />
+                        <OptionConfigLink key={text} text={text} icon={icon} action={action} setShowPanel={setShowPanel} />
                     )
                 })}
                 </div> : ''}
--- frontend/src/pages/Dashboard/MinifyUrl/OptionConfigLink/OptionConfigLink.jsx
+++ frontend/src/pages/Dashboard/MinifyUrl/OptionConfigLink/OptionConfigLink.jsx
@@ -1,8 +1,17 @@
 import './OptionConfigLink.css'
 
-export default function OptionConfigLink({text, icon}) {
+export default function OptionConfigLink({text, icon, action, setShowPanel}) {
+    function handleClick(e) {
+        e.stopPropagation()
+        if (action && typeof action === 'function'){
+            action()
+            setTimeout(() => {
+                setShowPanel(false)
+            }, 100)
+        }
+    }
     return (
-    <div className='option-config-container'>
+    <div className='option-config-container' onClick={handleClick}>
         <i className={`${icon} option-config-icon`}></i>
         <span className='option-config-text'>{text}</span>
     </div>
