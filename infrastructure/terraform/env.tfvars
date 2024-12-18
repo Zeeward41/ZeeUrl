8e6fda1c019ba40f03dee00eed0f88a91673ad70
Zeeward41 <zeeward41@gmail.com>
2024-12-18 16:59:10 +0100

    feat: Add Terraform configuration with S3 backend

---
+++ infrastructure/terraform/env.tfvars
@@ -0,0 +1,5 @@
+name         = "zeeward41"
+project_name = "zeeurl"
+environment  = "ci"
+region       = "eu-north-1"
+ami_instance       = "ami-08eb150f611ca277f"
---
+++ infrastructure/terraform/terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+    backend "s3" {
+        bucket = "zeeurl-backend-41"
+        region = "eu-north-1"
+        key    = "terraform.tfstate"
+    }
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "5.72.1"
+        }
+
+        tls = {
+          source = "hashicorp/tls"
+          version = "4.0.6"
+        }
+
+        local = {
+          source =  "hashicorp/local"
+          version = "2.5.2"
+        }
+    }
+}
