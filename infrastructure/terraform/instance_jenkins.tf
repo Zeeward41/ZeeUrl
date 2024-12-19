d9a8d5f73a706cd87f2781c8e8e63ba3338d5fe6
Zeeward41 <zeeward41@gmail.com>
2024-12-19 16:00:18 +0100

    typo workflow configuration + Security Group all allow ssh

--- infrastructure/terraform/instance_jenkins.tf
+++ infrastructure/terraform/instance_jenkins.tf
@@ -11,7 +11,8 @@ resource "aws_vpc_security_group_ingress_rule" "allow_github_ssh" {
   from_port         = 22
   to_port           = 22
   ip_protocol       = "tcp"
-  cidr_ipv4 = "86.248.17.40/32"
+  # cidr_ipv4 = "86.248.17.40/32"
+  cidr_ipv4 = "0.0.0.0/0"
   # cidr_ipv4 = jsondecode(data.http.github_actions_ip.response_body).actions
 }
 
