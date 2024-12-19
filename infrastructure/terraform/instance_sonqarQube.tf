aadc1dc4d25cc1b2f661a5598e159f01ec0919f1
Zeeward41 <zeeward41@gmail.com>
2024-12-19 18:20:52 +0100

    add Terraform Instance sonarQube

--- infrastructure/terraform/instance_sonqarQube.tf
+++ infrastructure/terraform/instance_sonqarQube.tf
@@ -1,13 +1,13 @@
 # EC2 Instance
 ## Security Groupe ALL/ALL
-resource "aws_security_group" "jenkins" {
-  name        = "jenkins-SG"
-  description = "Security Group for Jenkins Server"
+resource "aws_security_group" "sonarQube" {
+  name        = "sonarQube-SG"
+  description = "Security Group for sonarQube Server"
 }
 
 # Ingress rule
 resource "aws_vpc_security_group_ingress_rule" "allow_github_ssh" {
-  security_group_id = aws_security_group.jenkins.id
+  security_group_id = aws_security_group.sonarQube.id
   from_port         = 22
   to_port           = 22
   ip_protocol       = "tcp"
@@ -16,7 +16,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_github_ssh" {
 
 # Ingress rule
 resource "aws_vpc_security_group_ingress_rule" "allow_me_ssh" {
-  security_group_id = aws_security_group.jenkins.id
+  security_group_id = aws_security_group.sonarQube.id
   from_port         = 22
   to_port           = 22
   ip_protocol       = "tcp"
@@ -24,17 +24,17 @@ resource "aws_vpc_security_group_ingress_rule" "allow_me_ssh" {
 }
 
 # Ingress rule
-resource "aws_vpc_security_group_ingress_rule" "allow_me_jenkins" {
-  security_group_id = aws_security_group.jenkins.id
-  from_port         = 8080
-  to_port           = 8080
+resource "aws_vpc_security_group_ingress_rule" "allow_me_sonarQube" {
+  security_group_id = aws_security_group.sonarQube.id
+  from_port         = 9000
+  to_port           = 9000
   ip_protocol       = "tcp"
   cidr_ipv4 = var.mon_ip
 }
 
 # Egress rule
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
-  security_group_id = aws_security_group.jenkins.id
+  security_group_id = aws_security_group.sonarQube.id
   #   from_port         = 0
   #   to_port           = 0
   ip_protocol = "-1" # Allow all protocols
@@ -42,7 +42,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
 }
 
 ## Public Instance
-resource "aws_instance" "jenkins_public_server" {
+resource "aws_instance" "sonarQube_public_server" {
   ami                         = var.ami_instance
   instance_type               = var.instance_type
   associate_public_ip_address = true
@@ -51,9 +51,9 @@ resource "aws_instance" "jenkins_public_server" {
   key_name = aws_key_pair.key_ci_Zeeurl.key_name
 
   # Attach the security group
-  vpc_security_group_ids = [aws_security_group.jenkins.id]
+  vpc_security_group_ids = [aws_security_group.sonarQube.id]
 
   tags = {
-    Name = "Jenkins Server"
+    Name = "SonarQube Server"
   }
 }
