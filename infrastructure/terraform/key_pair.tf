3415f9d33f2d06aaa07acacedab0600a3e9e7056
Zeeward41 <zeeward41@gmail.com>
2024-12-20 18:00:41 +0100

    add sonarqube server

--- infrastructure/terraform/key_pair.tf
+++ infrastructure/terraform/key_pair.tf
@@ -10,9 +10,14 @@ resource "aws_key_pair" "key_ci_Zeeurl" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-resource "local_file" "dns_instance" {
+resource "local_file" "dns_instance_Jenkins" {
     content= aws_instance.jenkins_public_server.public_dns
-    filename = "dns_instance.txt"
+    filename = "dns_instance_jenkins.txt"
+}
+
+resource "local_file" "dns_instance_SonarQube" {
+    content= aws_instance.jenkins_public_server.public_dns
+    filename = "dns_instance_sonarqube.txt"
 }
 
 resource "local_file" "private_key" {
