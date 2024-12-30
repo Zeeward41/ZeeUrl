2c6a36c7386d6ac78d55c8bf0e3806a22def453d
Zeeward41 <zeeward41@gmail.com>
2024-12-30 17:56:07 +0100

    add CD

---
+++ infrastructure/CD/terraform/key_pair.tf
@@ -0,0 +1,12 @@
+# Générer une clé SSH
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Créer une ressource de clé AWS
+resource "aws_key_pair" "key_ci_Zeeurl" {
+  key_name   = "key_cd_kubernetes"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
