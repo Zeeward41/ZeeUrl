efafcbaeba11f5e4d067dded35c646a16ae6ae9d
Zeeward41 <zeeward41@gmail.com>
2025-01-06 19:28:32 +0100

    update: add host: 0.0.0.0 (vite accept external request)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: parseInt(process.env.VITE_FRONTEND_PORT), watch: {usePolling: true},
+  server: {port: process.env.VITE_FRONTEND_PORT, watch: {usePolling: true}, host: "0.0.0.0",
   // proxy: {
   //   '/api': {
   //     target: 'http://localhost:5000',
