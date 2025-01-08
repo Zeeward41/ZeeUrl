a5e130d84045d728817ccabe8379371ef73d48fe
Zeeward41 <zeeward41@gmail.com>
2025-01-08 12:39:04 +0100

    update: change vite.config.js - frontend - load env var

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {port: 3000, watch: {usePolling: true}, host: "0.0.0.0",
-  // proxy: {
-  //   '/api': {
-  //     target: 'http://localhost:5000',
-  //     changeOrigin: true
-  //   }
-  // }}
-},
-  preview: {
-    port: 3000,
-    host: "0.0.0.0"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: "0.0.0.0"
+    },
+    preview: {
+      port: 3000,
+      host: "0.0.0.0"
+    },
+    define: {
+      'process.env.VITE_BACKEND_API_URL': JSON.stringify(env.VITE_BACKEND_API_URL)
+    }
   }
 })
