65c8f620fcbb0bd73856efb7cb5b397222599fea
Zeeward41 <zeeward41@gmail.com>
2025-01-10 17:26:53 +0100

    update: test env var runtime frontend

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,7 @@
-export const API_BASE_URL = window._env_.VITE_BACKEND_API_URL || import.meta.env.VITE_BACKEND_API_URL
-export const WEB_SITE_URL = window._env_.VITE_FRONTEND_URL || import.meta.env.VITE_FRONTEND_URL
+// export const API_BASE_URL = window._env_.VITE_BACKEND_API_URL || import.meta.env.VITE_BACKEND_API_URL
+// export const WEB_SITE_URL = window._env_.VITE_FRONTEND_URL || import.meta.env.VITE_FRONTEND_URL
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_API_URL
+export const WEB_SITE_URL = import.meta.env.VITE_FRONTEND_URL
 export const ENDPOINTS = {
   REGISTER: `${API_BASE_URL}/v1/auth/register`,
   LOGIN: `${API_BASE_URL}/v1/auth/login`,
--- frontend/public/env.js
+++ frontend/public/env.js
@@ -1,22 +1,22 @@
 
-window._env_ = (function () {
-    const env = '${VITE_NODE_ENV}'; // Placeholder remplacé en production via entrypoint.sh
+// window._env_ = (function () {
+//     const env = '${VITE_NODE_ENV}'; // Placeholder remplacé en production via entrypoint.sh
   
-    if (env === 'development') {
-      // Environnement de développement : valeurs directement injectées
-      return {
-        VITE_NODE_ENV: 'development',
-        VITE_BACKEND_API_URL: '${VITE_BACKEND_API_URL}',
-        VITE_FRONTEND_URL: '${VITE_FRONTEND_URL}',
-      };
-    } else if (env === 'production') {
-      // Environnement de production : placeholders pour substitution
-      return {
-        VITE_NODE_ENV: 'production',
-        VITE_BACKEND_API_URL: '${VITE_BACKEND_API_URL}', // Substitué à l'exécution
-        VITE_FRONTEND_URL: '${VITE_FRONTEND_URL}',       
-      };
-    } else {
-      throw new Error(`Environnement inconnu : ${env}`);
-    }
-  })();
+//     if (env === 'development') {
+//       // Environnement de développement : valeurs directement injectées
+//       return {
+//         VITE_NODE_ENV: 'development',
+//         VITE_BACKEND_API_URL: '${VITE_BACKEND_API_URL}',
+//         VITE_FRONTEND_URL: '${VITE_FRONTEND_URL}',
+//       };
+//     } else if (env === 'production') {
+//       // Environnement de production : placeholders pour substitution
+//       return {
+//         VITE_NODE_ENV: 'production',
+//         VITE_BACKEND_API_URL: '${VITE_BACKEND_API_URL}', // Substitué à l'exécution
+//         VITE_FRONTEND_URL: '${VITE_FRONTEND_URL}',       
+//       };
+//     } else {
+//       throw new Error(`Environnement inconnu : ${env}`);
+//     }
+//   })();
