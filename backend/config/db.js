5bb13b36c0899304c64fbab3d45d6ee972f4a2a6
Zeeward41 <zeeward41@gmail.com>
2025-01-11 15:00:06 +0100

    fix: bakend typo Error :  error -> err

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold)
     } catch (err) {
-        console.log(`Error: ${error.message}`.red)
+        console.log(`Error: ${err.message}`.red)
         process.exit(1)
     }
 }
