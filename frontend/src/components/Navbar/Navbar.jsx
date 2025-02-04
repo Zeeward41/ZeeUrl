b6b87dc2b1cec2ec86dcf63049d5905959bb2ea1
Zeeward41 <zeeward41@gmail.com>
2025-02-04 17:35:23 +0100

    add: info-pane: This is a demo Project

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -17,22 +17,25 @@ function Navbar() {
         window.location.href = '/'
     }
     return (
-        <div className="nav-container">
-            <nav className="nav">
-                <Link onClick={handleClick} className="site-title">ZeeURL</Link>
-            {authState.isAuthenticated ? <ProfileButton />:
-                <ul>
-                    <li>
-                        <Link to="/signup" >Sign Up</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
-                }
-            </nav>
-            {showLogoutMessage ? <ModalMessage type={typeModal} message={messageModal}/> : ''}
-        </div>
+        <>
+            <div className="nav-container">
+                <nav className="nav">
+                    <Link onClick={handleClick} className="site-title">ZeeURL</Link>
+                {authState.isAuthenticated ? <ProfileButton />:
+                    <ul>
+                        <li>
+                            <Link to="/signup" >Sign Up</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                    }
+                </nav>
+                {showLogoutMessage ? <ModalMessage type={typeModal} message={messageModal}/> : ''}
+            </div>
+            <div className="info-panel">This is a DEMO project.</div>
+        </>
     )
    
 }
