7988260e6e8c09f37ba51be79bb65fed0b9ce236
Zeeward41 <zeeward41@gmail.com>
2025-02-08 14:08:51 +0100

    update: color website

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer className='footer'>
-            <p className='footer-reserved'>&copy; 2024 ZeeUrl. All rights reserved.</p>
+            <p className='footer-reserved'>&copy; 2025 ZeeUrl. All rights reserved.</p>
             <div className="footer-social-links">
                 <a href="https://x.com/zeeward41" className='footer-x'target="_blank"><i className='fa-brands fa-x-twitter'></i>Twitter</a>
                 <a href="https://github.com/Zeeward41" className='footer-github' target="_blank"><i className='fa-brands fa-github'></i>Github</a>
