b7383bb8a8c959bb51720a4507622aabc84f2a81
Zeeward41 <zeeward41@gmail.com>
2025-02-11 22:04:12 +0100

    fix: alb listener certificate_arn

--- infrastructure/CD/terraform/ALB_ingress.tf
+++ infrastructure/CD/terraform/ALB_ingress.tf
@@ -74,7 +74,8 @@ resource "aws_lb_listener" "https_listener" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn = aws_acm_certificate.zeeurl_acm.arn
+  # certificate_arn = aws_acm_certificate.zeeurl_acm.arn
+  certificate_arn = aws_acm_certificate_validation.zeeurl_cert_validate.certificate_arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.k8s_ingress_http.arn
--- infrastructure/CD/terraform/acm.tf
+++ infrastructure/CD/terraform/acm.tf
@@ -1,18 +1,8 @@
-# Provider global
-
-provider "aws" {
-    alias = "global"
-    region = "us-east-1"
-} 
-
 
 # Create a certificate
 resource "aws_acm_certificate" "zeeurl_acm" {
   domain_name       = var.web_site
   validation_method = "DNS"
-
-  //certificate must be in global region
-  provider = aws.global
 
   lifecycle {
     create_before_destroy = true
@@ -20,14 +10,11 @@ resource "aws_acm_certificate" "zeeurl_acm" {
 }
 
 # Validation certificate
-
 resource "aws_acm_certificate_validation" "zeeurl_cert_validate" {
-  provider                = aws.global
   certificate_arn         = aws_acm_certificate.zeeurl_acm.arn
-#   validation_record_fqdns = [aws_route53_record.zeeurl_cert_dns.fqdn]
   validation_record_fqdns = [for record in aws_route53_record.zeeurl_cert_dns : record.fqdn]
 
   timeouts {
-    create = "32m"
+    create = "15m"
   }
 }
