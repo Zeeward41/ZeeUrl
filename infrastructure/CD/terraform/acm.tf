102985233190157c7fd6d0552680fecfd3bdc896
Zeeward41 <zeeward41@gmail.com>
2025-02-11 15:05:49 +0100

    fix mistakes (provider global for acm), change region, name alb_listener identique (80, 443)

--- infrastructure/CD/terraform/acm.tf
+++ infrastructure/CD/terraform/acm.tf
@@ -1,20 +1,28 @@
+# Provider global
+
+provider "aws" {
+    alias = "global"
+    region = "us-east-1"
+} 
+
+
 # Create a certificate
 resource "aws_acm_certificate" "zeeurl_acm" {
-    domain_name = var.web_site
-    validation_method = "DNS"
+  domain_name       = var.web_site
+  validation_method = "DNS"
 
-    //certificate must be in global region
-    provider = aws.global
+  //certificate must be in global region
+  provider = aws.global
 
-    lifecycle {
-        create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Validation certificate
 
 resource "aws_acm_certificate_validation" "zeeurl_cert_validate" {
-    provider = aws.global
-    certificate_arn = aws_acm_certificate.zeeurl_acm.arn
-    validation_record_fqdns = [aws_route53_record.zeeurl_cert_dns.fqdn]
+  provider                = aws.global
+  certificate_arn         = aws_acm_certificate.zeeurl_acm.arn
+  validation_record_fqdns = [aws_route53_record.zeeurl_cert_dns.fqdn]
 }
